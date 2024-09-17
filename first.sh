246729a6399097aef9f3077d968c9c61caec2214
nafees <abc.com>
2024-09-17 15:46:26 +0530

    furstcommit

---
+++ first.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This is script for TWS
+
+echo "TWS: Hello Dosto"
+
+echo "Learners: DevOps Wale bhaiya , hum toh comment karenge"
+
+echo "TWS: Toh dosto, like bhi kar do"
+
+ls
+ls -al
+date
+time
